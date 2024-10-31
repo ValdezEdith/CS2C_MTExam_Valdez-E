49b9f388e42a5225df377c70ab431af2cd2f4d7f
ValdezEdith <zedlavhtide@gmail.com>
2024-10-31 09:08:12 +0800

    Update favNumber.js

--- favNumber.js
+++ favNumber.js
@@ -1,32 +1,5 @@
-// Collect user information
-let fullName = prompt("Enter your full name (first and last):");
-let age = prompt("Enter your age:");
-let favoriteNumber = prompt("Enter your favorite number:");
-let favoriteColor = prompt("Enter your favorite color:");
-
-// Log the information to the console
-console.log("Full Name:", fullName);
-console.log("Age:", age);
-console.log("Favorite Number:", favoriteNumber);
-console.log("Favorite Color:", favoriteColor);
-
-// Age Categorization
-if (age < 5) {
-  console.log(`${fullName} is a Toddler.`);
-} else if (age >= 5 && age <= 12) {
-  console.log(`${fullName} is a Child.`);
-} else if (age >= 13 && age <= 19) {
-  console.log(`${fullName} is a Teenager.`);
-} else if (age >= 20 && age <= 35) {
-  console.log(`${fullName} is a Young Adult.`);
-} else if (age >= 36 && age <= 60) {
-  console.log(`${fullName} is Middle-Aged.`);
-} else {
-  console.log(`${fullName} is a Senior.`);
-}
-
 // Favorite Number Calculation
-let theFavNumber = parseInt(favoriteNumber); // Convert favoriteNumber to an integer
+let theFavNumber = 19
 let guess;
 
 while (true) {
